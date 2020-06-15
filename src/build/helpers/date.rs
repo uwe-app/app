5de5a238cb6a396bc7c14b3e975b1bfd3526f322
muji <muji@tmpfs.org>
2020-06-15 15:23:08 +0800

    Update to draft date/time helper.

--- src/build/helpers/date.rs
+++ src/build/helpers/date.rs
@@ -1,9 +1,7 @@
 use handlebars::*;
 
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Utc, Local};
 use serde_json::{json, from_value};
-
-use super::map_render_error;
 
 #[derive(Clone, Copy)]
 pub struct DateFormat;
@@ -17,36 +15,31 @@ impl HelperDef for DateFormat{
         _rc: &mut RenderContext<'reg, 'rc>,
         out: &mut dyn Output,
     ) -> HelperResult {
+        // Use local=true to convert to local timezone
 
-        // TODO: support local=true to convert to local timezone
         // TODO: support format shortcuts for common formats
+        // TODO: support locale aware date/time formats
 
-        let date_err = Err(
-            RenderError::new(
-                "Type error for `date`, first parameter must be Utc date"));
+        let dt = h.param(0).map(|v| v.value())
+            .ok_or(RenderError::new("Type error for `date`, first parameter must be date"));
 
-        let format_err = Err(
-            RenderError::new(
-                "Type error for `date`, second parameter must be string format"));
+        let fmt = h.param(1).map(|v| v.value())
+            .ok_or(RenderError::new("Type error for `date`, second parameter must be format string"));
 
-        if let Some(dt) = h.params().get(0) {
-            if !dt.is_value_missing() {
-                let value = dt.value(); 
-                let date: DateTime<Utc> = from_value(value.clone())?;
-                if let Some(fmt) = h.params().get(1) {
-                    if !fmt.is_value_missing() {
-                        let value = fmt.value(); 
-                        if let Some(ref fmt) = value.as_str() {
-                            let format = date.format(fmt).to_string();
-                            out.write(&format)?;
-                        }
+        let local = h.hash_get("local").map(|v| v.value());
 
+        if let Ok(dt) = dt {
+            let date: DateTime<Utc> = from_value(dt.clone())?;
+            if let Ok(fmt) = fmt {
+                if let Some(fmt) = fmt.as_str() {
+                    let format = if let Some(local) = local {
+                        let converted: DateTime<Local> = DateTime::from(date);
+                        converted.format(fmt).to_string()
                     } else {
-                        return format_err; 
-                    }
+                        date.format(fmt).to_string()
+                    };
+                    out.write(&format)?;
                 }
-            } else {
-                return date_err
             }
         }
 
