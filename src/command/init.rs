abbcd6c419142570f51ef7d6dab371ad20e9c6f6
muji <muji@tmpfs.org>
2020-06-17 19:24:12 +0800

    WIP: for fresh repo history on init.

--- src/command/init.rs
+++ src/command/init.rs
@@ -4,7 +4,18 @@ use std::path::PathBuf;
 
 use home;
 use url::Url;
-use git2::Repository;
+use git2::{
+    Repository,
+    IndexAddOption,
+    Cred,
+    RemoteCallbacks,
+    FetchOptions, 
+    ErrorClass,
+    Config,
+    Signature,
+    Commit,
+};
+use git2::build::RepoBuilder;
 use log::info;
 
 use crate::blueprint;
@@ -22,6 +33,76 @@ pub struct InitOptions {
     pub private_key: Option<PathBuf>,
 }
 
+fn fresh<P: AsRef<Path>>(target: P, repo: Repository) -> Result<(), Error> {
+    let git_dir = repo.path();
+
+    println!("Fresh {}", git_dir.display());
+
+    // Remove the git directory is the easiest
+    // way to purge the history
+    fs::remove_dir_all(git_dir)?;
+
+    // Create fresh repository
+    let new_repo = Repository::init(target)?;
+
+    println!("After init new repo {}", new_repo.path().display());
+
+    // Add all the files
+    let mut index = new_repo.index()?;
+    index.add_all(["*"].iter(), IndexAddOption::DEFAULT, None)?;
+    let oid = index.write_tree()?;
+
+    println!("After adding files to the index");
+
+    let conf = Config::open_default()?;
+
+    println!("Got config");
+
+    // TODO: get these from preferences when not setand use defaults otherwise
+    let name = conf.get_string("user.name")?;
+    let email = conf.get_string("user.email")?;
+
+    println!("Got name {}", &name);
+    println!("Got email {}", &email);
+
+    let author = Signature::now(&name, &email)?;
+    let committer = Signature::now(&name, &email)?;
+
+    // TODO: allow prefernce for this
+    let message = "Initial files.";
+
+    //let head = new_repo.head()?;
+
+    println!("Got head reference");
+
+    //if let Some(oid) = head.target() {
+
+        println!("Got oid {:?}", oid);
+
+        let tree = new_repo.find_tree(oid)?;
+
+        println!("Found the tree {:?}", tree);
+
+        let parents: &[&Commit] = &[];
+
+        println!("Making the commit to the repo");
+
+        let commit_id = new_repo.commit(
+            None,
+            &author,
+            &committer,
+            message,
+            &tree,
+            parents,
+        )?;
+
+        println!("Got commit it {:?}", commit_id);
+    //}
+
+
+    Ok(())
+}
+
 fn clone_ssh<P: AsRef<Path>>(
     src: String,
     target: P,
@@ -37,9 +118,9 @@ fn clone_ssh<P: AsRef<Path>>(
 
     let private_key = key_file.as_path();
 
-    let mut callbacks = git2::RemoteCallbacks::new();
+    let mut callbacks = RemoteCallbacks::new();
     callbacks.credentials(|_url, username_from_url, _allowed_types| {
-        git2::Cred::ssh_key(
+        Cred::ssh_key(
             username_from_url.unwrap(),
             None,
             private_key,
@@ -47,10 +128,10 @@ fn clone_ssh<P: AsRef<Path>>(
         )
     });
 
-    let mut fo = git2::FetchOptions::new();
+    let mut fo = FetchOptions::new();
     fo.remote_callbacks(callbacks);
 
-    let mut builder = git2::build::RepoBuilder::new();
+    let mut builder = RepoBuilder::new();
     builder.fetch_options(fo);
 
     let result = builder.clone(
@@ -59,7 +140,7 @@ fn clone_ssh<P: AsRef<Path>>(
     );
 
     if let Err(ref e) = result {
-        if let git2::ErrorClass::Ssh = e.class() {
+        if let ErrorClass::Ssh = e.class() {
             // Sadly cannot find a better way to detect this particular error
             if e.message().contains("Wrong passphrase") {
                 let pass = rpassword::read_password_from_tty(Some("Passphrase: "))?;
@@ -176,6 +257,7 @@ pub fn init(options: InitOptions) -> Result<(), Error> {
             }
 
             let repo;
+            let fresh_target = target.clone();
 
             if let Some(ref parent) = target.parent() {
                 if !parent.exists() {
@@ -186,7 +268,9 @@ pub fn init(options: InitOptions) -> Result<(), Error> {
                 repo = create(target, &options, &prefs)?;
             }
 
-            repo.remote_delete("origin")?;
+            //repo.remote_delete("origin")?;
+
+            fresh(&fresh_target, repo)?;
         } else {
             return Err(Error::new(format!("Target directory is required")));
         }
