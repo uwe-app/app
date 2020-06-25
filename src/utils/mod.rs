e772c62ea16493570d630c167f6d2c4c321eca2d
muji <muji@tmpfs.org>
2020-06-25 17:15:30 +0800

    Tweak declaration.

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -24,7 +24,7 @@ pub mod symlink;
 pub mod url;
 
 pub fn generate_id(len: i32) -> String {
-    let mut s: String = "".to_owned();
+    let mut s = "".to_string();
     for _ in 0..len {
         let x = rand::random::<u8>();
         s.push_str(&format!("{:x}", x));
