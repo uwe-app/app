24655ac7cfa3082c04b4cfc03662d27d6d1f8ad8
muji <muji@tmpfs.org>
2020-07-01 09:33:11 +0800

    Move struct to mod.rs.

--- src/workspace/mod.rs
+++ src/workspace/mod.rs
@@ -1,10 +1,21 @@
+use config::Config;
+
 mod compile;
 mod finder;
 pub mod project;
-mod types;
+
+#[derive(Clone)]
+pub struct Workspace {
+    pub config: Config,
+}
+
+impl Workspace {
+    pub fn new(config: Config) -> Self {
+        Self { config }
+    }
+}
 
 pub use finder::find;
 pub use compile::compile;
 pub use compile::compile_from;
 pub use compile::compile_project;
-pub use types::Workspace;
