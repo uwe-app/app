55650de1cc03db62f98768608791b9d3a4140f50
muji <muji@tmpfs.org>
2020-07-21 11:16:35 +0800

    Tidy link helper.
    And fix for links to root when rewrite index is disabled, closes #95.

--- components/compiler/src/helpers/link.rs
+++ components/compiler/src/helpers/link.rs
@@ -27,7 +27,7 @@ impl HelperDef for Link<'_> {
             .evaluate(ctx, "@root/file.source")?
             .as_json()
             .as_str()
-            .ok_or_else(|| RenderError::new("Type error for `file`, string expected"))?
+            .ok_or_else(|| RenderError::new("Type error for `file.source`, string expected"))?
             .replace("\"", "");
 
         let types = self.context.options.settings.types.as_ref().unwrap();
@@ -96,19 +96,25 @@ impl HelperDef for Link<'_> {
             }
 
             if let Ok(rel) = path.strip_prefix(base) {
+                let up = "../";
                 let mut value: String = "".to_string();
                 if let Some(p) = rel.parent() {
                     if opts.settings.should_rewrite_index() && FileInfo::is_clean(&path, types) {
-                        value.push_str("../");
+                        value.push_str(up);
                     }
                     for _ in p.components() {
-                        value.push_str("../");
+                        value.push_str(up);
                     }
                 }
 
                 value.push_str(&input);
+
                 if include_index && (value.ends_with("/") || value == "") {
                     value.push_str(config::INDEX_HTML);
+                }
+
+                if !opts.settings.should_rewrite_index() && value == "" {
+                    value = up.to_string();
                 }
 
                 debug!("Link {:?}", value);
@@ -123,86 +129,6 @@ impl HelperDef for Link<'_> {
             return Err(RenderError::new(
                 "Type error for `link`, expected string parameter",
             ));
-        }
-        Ok(())
-    }
-}
-
-#[derive(Clone, Copy)]
-pub struct Match<'a> {
-    pub context: &'a BuildContext
-}
-
-impl HelperDef for Match<'_> {
-    fn call<'reg: 'rc, 'rc>(
-        &self,
-        h: &Helper<'reg, 'rc>,
-        _r: &'reg Handlebars<'_>,
-        ctx: &'rc Context,
-        rc: &mut RenderContext<'reg, 'rc>,
-        out: &mut dyn Output,
-    ) -> HelperResult {
-
-        let base_path = rc
-            .evaluate(ctx, "@root/file.target")?
-            .as_json()
-            .as_str()
-            .ok_or_else(|| RenderError::new("Type error for `file.target`, string expected"))?
-            .replace("\"", "");
-
-        let opts = &self.context.options;
-        let path = Path::new(&base_path).to_path_buf();
-
-        if h.params().len() != 2 && h.params().len() != 3 {
-            return Err(RenderError::new(
-                "Type error for `match`, two parameters expected",
-            ));
-        }
-
-        let mut target = "".to_owned();
-        let mut output = "".to_owned();
-        let mut exact = false;
-
-        if let Some(p) = h.params().get(0) {
-            if !p.is_value_missing() {
-                target = p.value().as_str().unwrap_or("").to_string();
-            }
-        }
-
-        if target.ends_with("/") {
-            target = target.trim_end_matches("/").to_string();
-        }
-
-        if let Some(p) = h.params().get(1) {
-            if !p.is_value_missing() {
-                output = p.value().as_str().unwrap_or("").to_string();
-            }
-        }
-
-        if let Some(p) = h.params().get(2) {
-            if !p.is_value_missing() {
-                exact = p.value().as_bool().unwrap_or(true);
-            }
-        }
-
-        if let Ok(rel) = path.strip_prefix(&opts.target) {
-            let mut pth = "".to_string();
-            pth.push('/');
-            pth.push_str(&rel.to_string_lossy().into_owned());
-            if pth.ends_with(config::INDEX_HTML) {
-                pth = pth.trim_end_matches(config::INDEX_HTML).to_string();
-            }
-            if pth.ends_with("/") {
-                pth = pth.trim_end_matches("/").to_string();
-            }
-
-            let matches = (exact && pth == target)
-                || (!exact && target != "" && pth.starts_with(&target))
-                || (!exact && target == "" && pth == "");
-
-            if matches {
-                out.write(&output)?;
-            }
         }
         Ok(())
     }
