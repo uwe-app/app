fa795ab8f9fc79462d7dc0b026ec7612cbe5f72d
muji <muji@tmpfs.org>
2020-07-22 05:11:21 +0800

    Tidy children helper.

--- components/compiler/src/helpers/children.rs
+++ components/compiler/src/helpers/children.rs
@@ -20,12 +20,16 @@ impl HelperDef for Children<'_> {
         rc: &mut RenderContext<'reg, 'rc>,
         out: &mut dyn Output,
     ) -> HelperResult {
+
         let base_path = rc
             .evaluate(ctx, "@root/file.source")?
             .as_json()
             .as_str()
             .ok_or_else(|| RenderError::new("Type error for `file.source`, string expected"))?
             .replace("\"", "");
+
+        let template = h.template()
+            .ok_or_else(|| RenderError::new("Type error in `children`, block template expected"))?;
 
         let path = Path::new(&base_path).to_path_buf();
         let dir = path.parent().unwrap().to_path_buf();
@@ -41,26 +45,21 @@ impl HelperDef for Children<'_> {
         let list_result = tree::listing(self.context, &list_opts);
         match list_result {
             Ok(entries) => {
-                let template = h.template();
-                match template {
-                    Some(t) => {
-                        for li in entries {
-                            let mut local_rc = rc.clone();
-                            let mut local_ctx = Context::wraps(li)?;
-                            if let Some(ref file_ctx) = li.file {
-                                if file_ctx.source == path {
-                                    local_ctx.data_mut()
-                                        .as_object_mut()
-                                        .unwrap()
-                                        .insert("self".to_string(), Value::Bool(true));
-                                }
-                            }
-                            t.render(r, &local_ctx, &mut local_rc, out)?;
+
+                for li in entries {
+                    let mut local_rc = rc.clone();
+                    let mut local_ctx = Context::wraps(li)?;
+                    if let Some(ref file_ctx) = li.file {
+                        if file_ctx.source == path {
+                            local_ctx.data_mut()
+                                .as_object_mut()
+                                .unwrap()
+                                .insert("self".to_string(), Value::Bool(true));
                         }
-                        return Ok(());
                     }
-                    None => return Ok(()),
+                    template.render(r, &local_ctx, &mut local_rc, out)?;
                 }
+                return Ok(());
             }
             // FIXME: find a better way to convert these errors
             // SEE: https://stackoverflow.com/a/58337971/7625589
