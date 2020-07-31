b3290ba5348d25c528cc55f1c2df9ea29ddab607
muji <muji@tmpfs.org>
2020-07-31 14:46:24 +0800

    Support abs hash parameter in link helper.
    Also tidy the helper implementation for parameter handling.

--- components/compiler/src/helpers/link.rs
+++ components/compiler/src/helpers/link.rs
@@ -2,6 +2,7 @@ use std::path::Path;
 
 use handlebars::*;
 use log::debug;
+use serde_json::json;
 
 use config::FileInfo;
 
@@ -24,6 +25,14 @@ impl HelperDef for Link<'_> {
         out: &mut dyn Output,
     ) -> HelperResult {
 
+        let abs = h.hash_get("abs")
+            .map(|v| v.value())
+            .or(Some(&json!(false)))
+            .and_then(|v| v.as_bool())
+            .ok_or(RenderError::new(
+                "Type error for `link` helper, hash parameter `abs` must be a boolean"
+            ))?;
+
         let base_path = rc
             .evaluate(ctx, "@root/file.source")?
             .as_json()
@@ -36,67 +45,61 @@ impl HelperDef for Link<'_> {
         let opts = &self.context.options;
         let path = Path::new(&base_path);
 
-        let mut input: String = "".to_string();
+        let mut input = h.params().get(0)
+            .ok_or_else(|| RenderError::new("Type error in `link`, expected parameter"))?
+            .value()
+            .as_str()
+            .ok_or_else(|| RenderError::new("Type error in `link`, expected string parameter"))?;
 
-        if let Some(p) = h.params().get(0) {
-            let link_config = self.context.config.link.as_ref().unwrap();
-            let include_index = opts.settings.should_include_index();
+        let link_config = self.context.config.link.as_ref().unwrap();
+        let include_index = opts.settings.should_include_index();
+        let make_relative = !abs && link_config.relative.is_some() && link_config.relative.unwrap();
 
-            if !p.is_value_missing() {
-                input = p.value().as_str().unwrap_or("").to_string();
+        let passthrough =
+            !input.starts_with("/")
+            || input.starts_with("http:")
+            || input.starts_with("https:");
+
+        if passthrough {
+            out.write(&input)?;
+            if include_index && (input == "." || input == "..") {
+                out.write("/")?;
+                out.write(config::INDEX_HTML)?;
             }
+            return Ok(());
+        }
 
-            if input.is_empty() {
-                return Err(RenderError::new(
-                    "Type error for `link`, expected string parameter",
-                ));
-            }
+        // Strip the leading slash
+        if input.starts_with("/") {
+            input = input.trim_start_matches("/");
+        }
 
-            // Check config first
-            let enabled = link_config.relative.is_some() && link_config.relative.unwrap();
-            let passthrough = !enabled
-                || !input.starts_with("/")
-                || input.starts_with("http:")
-                || input.starts_with("https:");
+        let mut base = opts.source.clone();
 
-            if passthrough {
-                out.write(&input)?;
-                if include_index && (input == "." || input == "..") {
-                    out.write("/")?;
-                    out.write(config::INDEX_HTML)?;
-                }
-                return Ok(());
-            }
-
-            // Strip the leading slash
-            if input.starts_with("/") {
-                input = input.trim_start_matches("/").to_owned();
-            }
-
-            let mut base = opts.source.clone();
-
-            if let Some(verify) = link_config.verify {
-                if verify {
-                    //println!("Verify with input {:?}", &input);
-                    if !lookup::exists(self.context, &input) {
-                        return Err(RenderError::new(format!(
-                            "Type error for `link`, missing url {}",
-                            input
-                        )));
-                    }
+        if let Some(verify) = link_config.verify {
+            if verify {
+                //println!("Verify with input {:?}", &input);
+                if !lookup::exists(self.context, &input) {
+                    return Err(RenderError::new(format!(
+                        "Type error for `link`, missing url {}",
+                        input
+                    )));
                 }
             }
+        }
 
-            if let Some(ref href_path) = opts.settings.base_href {
-                base.push(href_path);
+        if let Some(ref href_path) = opts.settings.base_href {
+            base.push(href_path);
 
-                if input.starts_with(href_path) {
-                    input = input.trim_start_matches(href_path).to_owned();
-                    input = input.trim_start_matches("/").to_owned();
-                }
+            if input.starts_with(href_path) {
+                input = input.trim_start_matches(href_path);
+                input = input.trim_start_matches("/");
             }
+        }
 
-            if let Ok(rel) = path.strip_prefix(base) {
+        if let Ok(rel) = path.strip_prefix(base) {
+
+            let value = if make_relative {
                 let up = "../";
                 let mut value: String = "".to_string();
                 if let Some(p) = rel.parent() {
@@ -117,20 +120,20 @@ impl HelperDef for Link<'_> {
                 if !opts.settings.should_rewrite_index() && value == "" {
                     value = up.to_string();
                 }
-
-                debug!("Link {:?}", value);
-
-                out.write(&value)?;
+                value
             } else {
-                return Err(RenderError::new(
-                    "Type error for `link`, file is outside source!",
-                ));
-            }
+                format!("/{}", input)
+            };
+
+            debug!("Link {:?}", value);
+
+            out.write(&value)?;
         } else {
             return Err(RenderError::new(
-                "Type error for `link`, expected string parameter",
+                "Type error for `link`, file is outside source!",
             ));
         }
+
         Ok(())
     }
 }
