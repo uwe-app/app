aa6512692d3b019303f9f4e46efcef7d2ba924bb
muji <muji@tmpfs.org>
2020-08-06 15:06:41 +0800

    Cross-compilation for macos.
    Finally get it working, see `build_osx.sh`. Had to switch to
    `regex-fancy` from `regex-onig` in the syntect dependency which will
    slow down the syntax highlighting library.
    
    Later it would be good to restore onig if we can get the clang linker to
    find the onig symbols.
    
    An important change was the switch to using `vendored-openssl` feature
    for `git2` so that openssl is built from source for the target platform.
    
    Requires that `osxcross` has been built and target/bin is available in PATH.

--- build_osx.sh
+++ build_osx.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
-export GIT_USE_NSEC=""
 export PATH=$HOME/git/2.external/osxcross/target/bin:$PATH
 export PKG_CONFIG_ALLOW_CROSS=1
+export CC=o64-clang
+export CXX=o64-clang++
+export LIBZ_SYS_STATIC=1
+#export ONIG_STATIC=1
 cargo build --target=x86_64-apple-darwin --release
