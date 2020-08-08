e321a4e32b4748c26271ca8bccaff57f46a49a74
muji <muji@tmpfs.org>
2020-08-08 12:11:23 +0800

    Using new webassembly search binding.

--- components/search/src/lib.rs
+++ components/search/src/lib.rs
@@ -4,14 +4,18 @@ pub mod index;
 pub mod searcher;
 
 use thiserror::Error;
+use serde::{Deserialize, Serialize};
 
 use common::IndexFromFile;
 use config::Config;
-use searcher::index_analyzer::parse_index_version;
+use searcher::index_analyzer::{parse_index_version, IndexVersion};
 use searcher::SearchError;
+
+use std::convert::TryFrom;
 
 use index::v1 as LatestVersion;
 pub use LatestVersion::*;
+
 
 #[derive(Error, Debug)]
 pub enum Error {
@@ -35,27 +39,64 @@ extern "C" {
     fn log(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn wasm_search(index: &IndexFromFile, query: String, options: String) -> String {
-    console_error_panic_hook::set_once();
-    let opts: QueryOptions = serde_json::from_str(&options).unwrap_or(Default::default());
-    let search_result = search(index, query, &opts).and_then(|output| {
-        serde_json::to_string(&output).map_err(|_e| SearchError::JSONSerializationError)
-    });
-
-    match search_result {
-        Ok(string) => string,
-        Err(e) => format!("{{error: '{}'}}", e),
-    }
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SearchIndexInfo {
+    pub name: String,
+    pub size: usize,
+    pub version: String,
 }
 
 #[wasm_bindgen]
-pub fn get_index_version(index: &IndexFromFile) -> String {
-    let parse_result = parse_index_version(index);
+#[derive(Debug)]
+pub struct SearchIndex {
+    file: Vec<u8>,
+    index: Index,
+    version: IndexVersion,
+    options: QueryOptions,
+}
 
-    match parse_result {
-        Ok(v) => format!("{}", v),
-        Err(e) => format!("{}", e),
+#[wasm_bindgen]
+impl SearchIndex {
+    #[wasm_bindgen(constructor)]
+    pub fn new(file: &IndexFromFile, opts: String) -> Self {
+        console_error_panic_hook::set_once();
+
+        let options: QueryOptions =
+            serde_json::from_str(&opts).unwrap_or(Default::default());
+
+        // FIXME: handle errors gracefully
+        let version = parse_index_version(file).unwrap();
+        let index = Index::try_from(file).unwrap();
+
+        Self {
+            file: file.to_vec(),
+            version,
+            index,
+            options,
+        }
+    }
+
+    pub fn print(&self, name: String) {
+        let info = SearchIndexInfo {
+            name,
+            size: self.file.len(),
+            version: self.version.to_string(),
+        };
+        let msg = serde_json::to_string(&info).unwrap();
+        log(&msg);
+    }
+
+    pub fn search(&self, query: String) -> String {
+        log(&format!("Index search is running with query {}", query));
+
+        let search_result = search(&self.file, query, &self.options).and_then(|output| {
+            serde_json::to_string(&output).map_err(|_e| SearchError::JSONSerializationError)
+        });
+
+        match search_result {
+            Ok(res) => res,
+            Err(e) => format!("{{error: '{}'}}", e),
+        }
     }
 }
 
