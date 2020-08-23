a2bae5add0fe3ecae5aa8cfd806c6b9c62745ffd
muji <muji@tmpfs.org>
2020-08-23 14:52:24 +0800

    Remove obsolete code.

--- components/compiler/src/build.rs
+++ components/compiler/src/build.rs
@@ -42,50 +42,6 @@ impl<'a> Compiler<'a> {
         Ok(())
     }
 
-    /*
-    pub fn resolve(&self, file: &PathBuf) -> Option<&Page> {
-        let mut page: Option<&Page> = None;
-
-        // Check for a standard page
-        if let Some(default_page) = self.context.collation.pages.get(file) {
-            page = Some(default_page);
-        }
-
-        // Check for a locale specific override
-        if let Some(locale_pages) = self.context.collation.locale_pages.get(
-            &self.context.options.lang) {
-            if let Some(locale_page) = locale_pages.get(file) {
-                page = Some(locale_page);
-            }
-        }
-
-        page
-    }
-
-    // Build a single file
-    pub async fn one(&self, parser: &Parser<'_>, file: &PathBuf) -> Result<Option<ParseData>> {
-
-        // FIXME: support locale overrides such as `about.id.md`
-        // SEE: #160
-
-        //println!("Got file {:?}", file);
-        //println!("Got file {:?}", self.resolve(file));
-        
-        if let Some(page) = self.resolve(file) {
-            let render = page.render.is_some() && page.render.unwrap();
-            if !render {
-                let file_ctx = page.file.as_ref().unwrap();
-                return run::copy(file, &file_ctx.target).await;
-            }
-
-            run::parse(self.context, parser, page.get_template(), page).await
-        } else {
-            let target = self.context.collation.targets.get(file).unwrap();
-            run::copy(file, target).await
-        }
-    }
-    */
-
     pub fn resolve(&self, file: &PathBuf) -> Option<&Page> {
         let mut page: Option<&Page> = None;
 
