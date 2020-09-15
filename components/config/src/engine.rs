1d8dc1b9432928a0e247c930b4cfc3551db1fe1f
muji <muji@tmpfs.org>
2020-09-15 10:53:02 +0800

    Implement Display for TemplateEngine.
    So that we can use the string name as a path component when resolving
    runtime dependencies such as feed and book template files.

--- components/config/src/engine.rs
+++ components/config/src/engine.rs
@@ -1,6 +1,13 @@
+use std::fmt;
 use serde::{Serialize, Deserialize};
 
 /// The supported template engines.
+///
+/// Note that the strings of these enum values returned using 
+/// to_string() are used to resolve runtime dependencies 
+/// from cache components and must therefore be safe to use 
+/// as a file system path component.
+///
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum TemplateEngine {
     #[serde(rename = "handlebars")]
@@ -10,5 +17,15 @@ pub enum TemplateEngine {
 impl Default for TemplateEngine {
     fn default() -> Self {
         Self::Handlebars
+    }
+}
+
+impl fmt::Display for TemplateEngine {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Self::Handlebars => {
+                write!(f, "{}", "handlebars")
+            } 
+        }
     }
 }
