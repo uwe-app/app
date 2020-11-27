29b264c3c44a171680c2e1a1679e705cc29a4b6b
muji <muji@tmpfs.org>
2020-11-27 12:31:39 +0800

    Update include file to support raw paths.

--- components/compiler/src/hbs/helpers/include.rs
+++ components/compiler/src/hbs/helpers/include.rs
@@ -12,36 +12,33 @@ impl Helper for Include {
         template: Option<&'render Node<'render>>,
     ) -> HelperValue {
 
+        ctx.arity(1..1)?;
+
         let base_path = rc.try_evaluate("@root/file.source", &[Type::String])?
             .as_str()
-            .unwrap()
-            .to_string();
+            .unwrap();
 
         // TODO: support embedding only certain lines only
+        let mut buf = Path::new(base_path).to_path_buf();
 
-        let mut buf = Path::new(&base_path).to_path_buf();
+        // NOTE: this allows quoted strings and raw paths
+        if let Some(include_file) = ctx.get_fallback(0) {
+            let include_file = ctx.try_value(include_file, &[Type::String])?
+                .as_str()
+                .unwrap();
 
-        if let Some(parent) = buf.parent() {
-            buf = parent.to_path_buf();
-            let val = ctx.try_get(0, &[Type::String])?.as_str().unwrap();
-            //if let Some(req) = h.params().get(0) {
-                // TODO: support using "value()" too?
-                //if let Some(val) = req.relative_path() {
-                    buf.push(val);
-                    let result = utils::fs::read_string(&buf);
-                    match result {
-                        Ok(s) => {
-                            rc.write(&s)?;
-                        }
-                        Err(_) => {
-                            return Err(HelperError::new(format!(
-                                "Failed to read from include file: {}",
-                                buf.display()
-                            )))
-                        }
-                    }
-                //}
-            //}
+            if let Some(parent) = buf.parent() {
+                buf = parent.to_path_buf();
+                buf.push(include_file);
+                let result = utils::fs::read_string(&buf).map_err(|e| {
+                    HelperError::new(format!(
+                        "Failed to read from include file: {}",
+                        buf.display()
+                    ))
+                })?;
+                rc.write(&result)?;
+            }
+
         }
 
         Ok(None)
