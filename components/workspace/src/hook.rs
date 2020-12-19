b01c82e0cbb67d004aacfba9efab63c2cec27023
muji <muji@tmpfs.org>
2020-12-19 13:45:32 +0800

    Remove hook_root/BUILD_HOOK.
    No longer relevant now that hooks cannot be executed from plugins.

--- components/workspace/src/hook.rs
+++ components/workspace/src/hook.rs
@@ -22,24 +22,15 @@ pub fn exec(ctx: &Arc<BuildContext>, hook: &HookConfig, changed: Option<&PathBuf
         Error::CanonicalProjectRoot(ctx.config.project().to_path_buf())
     })?;
 
-    let hook_root = hook
-        .base()
-        .canonicalize()
-        .map_err(|_| Error::CanonicalProjectRoot(hook.base().to_path_buf()))?;
-
-    debug!("Hook root {}", hook_root.display());
-
     let mut cmd = hook.path.clone();
     let mut args: Vec<String> = vec![];
     if let Some(arguments) = &hook.args {
         args = arguments.to_vec();
     }
 
-    //let cmd_path = PathBuf::from(cmd);
-
     // Looks like a relative command, resolve to the project root
     if cmd.starts_with(".") {
-        let mut buf = hook_root.clone();
+        let mut buf = project_root.clone();
         buf.push(cmd.clone());
         cmd = buf.to_string_lossy().into_owned();
     }
@@ -58,18 +49,16 @@ pub fn exec(ctx: &Arc<BuildContext>, hook: &HookConfig, changed: Option<&PathBuf
     debug!("BUILD_PROJECT {}", project_root.display());
     debug!("BUILD_SOURCE {}", build_source.display());
     debug!("BUILD_TARGET {}", build_target.display());
-    debug!("BUILD_HOOK {}", hook_root.display());
     debug!("NODE_ENV {}", &node_env);
 
     let mut command = Command::new(cmd);
 
     command
-        .current_dir(&hook_root)
+        .current_dir(&project_root)
         .env("NODE_ENV", node_env)
         .env("BUILD_PROJECT", project_root.to_string_lossy().into_owned())
         .env("BUILD_SOURCE", build_source.to_string_lossy().into_owned())
         .env("BUILD_TARGET", build_target.to_string_lossy().into_owned())
-        .env("BUILD_HOOK", hook_root.to_string_lossy().into_owned())
         .args(args);
 
     if let Some(file) = changed {
