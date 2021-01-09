41ae1dcdf9271397d28c8f423bb66f17bed7ad7f
muji <muji@tmpfs.org>
2021-01-09 09:59:19 +0800

    Update release manifest.
    Convert to a manifest with latest and a Vec of other versions so that
    the website for releases.uwe.app can order correctly.

--- components/release/src/releases.rs
+++ components/release/src/releases.rs
@@ -1,6 +1,5 @@
 use std::collections::{BTreeMap, HashMap};
 use std::fs::{self, File};
-use std::io::Write;
 use std::path::{Path, PathBuf};
 use std::cmp::Ordering;
 use std::str::FromStr;
@@ -19,6 +18,8 @@ pub static LATEST: &str = "latest";
 
 pub static LINUX: &str = "linux";
 pub static MACOS: &str = "macos";
+#[cfg(target_os = "windows")]
+pub static WINDOWS: &str = "windows";
 
 pub static PUBLISH_EXE_NAMES: [&str; 5] = ["uwe", "upm", "uvm", "uwe-shim", "upm-shim"];
 pub static INSTALL_EXE_NAMES: [&str; 2] = ["uwe", "upm"];
@@ -27,13 +28,9 @@ pub static INSTALL_SHIM_NAMES: [&str; 2] = ["uwe-shim", "upm-shim"];
 
 pub static SHIM: [(&str, &str); 2] = [("uwe-shim", "uwe"), ("upm-shim", "upm")];
 
-#[cfg(target_os = "windows")]
-pub static WINDOWS: &str = "windows";
-
 pub(crate) type Platform = String;
-pub(crate) type ExecutableName = String;
 pub(crate) type ExecutableTargets =
-    BTreeMap<Platform, BTreeMap<ExecutableName, ExecutableArtifact>>;
+    BTreeMap<Platform, BTreeMap<String, ExecutableArtifact>>;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct ExecutableArtifact {
@@ -43,7 +40,6 @@ pub struct ExecutableArtifact {
     pub(crate) digest: Vec<u8>,
     pub(crate) size: u64,
 }
-
 
 fn to_hex<S>(digest: &Vec<u8>, serializer: S) -> std::result::Result<S::Ok, S::Error>
     where S: Serializer
@@ -83,12 +79,57 @@ pub fn shim_map() -> HashMap<String, String> {
         .collect::<HashMap<_, _>>()
 }
 
-#[serde_as]
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Clone)]
 pub struct Releases {
-    #[serde_as(as = "BTreeMap<DisplayFromStr,_>")]
     pub(crate) versions: BTreeMap<ReleaseVersion, ReleaseInfo>,
 }
+
+impl From<ReleaseManifest> for Releases {
+    fn from(manifest: ReleaseManifest) -> Self {
+        let mut versions = BTreeMap::new();
+        if let Some(mut info) = manifest.latest {
+            let semver = info.version.take().unwrap();
+            versions.insert(ReleaseVersion::from(semver), info);
+        }
+        manifest.versions
+            .into_iter()
+            .for_each(|mut info| {
+                let semver = info.version.take().unwrap();
+                versions.insert(ReleaseVersion::from(semver), info);
+            });
+        Releases {versions}
+    }
+}
+
+impl Releases {
+
+    pub fn is_empty(&self) -> bool {
+        self.versions.is_empty() 
+    }
+
+    pub fn latest(&self) -> (&Version, &ReleaseInfo) {
+        let (k, v) = self.versions.iter().take(1).next().unwrap();
+        (k.semver(), v)
+    }
+
+    pub fn contains(&self, version: &Version) -> bool {
+        let release_version = ReleaseVersion::from(version);
+        self.versions.contains_key(&release_version)
+    }
+
+    pub fn filter(self, version: Option<VersionReq>) -> Self {
+        if let Some(ref version) = version {
+            let versions = self.versions
+                .into_iter()
+                .filter(|(v, _)| version.matches(v.semver()))
+                .collect::<BTreeMap<_, _>>();
+
+            return Releases{ versions }
+        }
+        self
+    }
+}
+
 
 /// Wrapper for the version so we order releases in the 
 /// reverse direction with the latest as the first element.
@@ -96,11 +137,8 @@ pub struct Releases {
 /// This is required because the generated manifest is used 
 /// as a collections data source for the releases.uwe.app 
 /// website and we want to show the most recent release first.
-#[serde_as]
-#[derive(Debug, Serialize, Deserialize, Clone, Eq)]
+#[derive(Debug, Clone, Eq)]
 pub struct ReleaseVersion {
-    #[serde_as(as = "DisplayFromStr")]
-    #[serde(flatten)]
     version: Version,
 }
 
@@ -167,39 +205,39 @@ impl PartialEq for ReleaseVersion {
     }
 }
 
-impl Releases {
+#[derive(Debug, Serialize, Deserialize, Clone, Default)]
+#[serde(default)]
+pub struct ReleaseManifest {
+    latest: Option<ReleaseInfo>,
+    versions: Vec<ReleaseInfo>,
+}
 
-    pub fn is_empty(&self) -> bool {
-        self.versions.is_empty() 
-    }
+impl From<&Releases> for ReleaseManifest {
+    fn from(releases: &Releases) -> Self {
+        let releases = releases.clone();
+        let mut manifest: ReleaseManifest = Default::default();
+        let mut it = releases.versions.into_iter();
 
-    pub fn latest(&self) -> (&Version, &ReleaseInfo) {
-        //self.versions.iter().rev().take(1).next().unwrap()
-        let (k, v) = self.versions.iter().take(1).next().unwrap();
-        (k.semver(), v)
-    }
-
-    pub fn contains(&self, version: &Version) -> bool {
-        let release_version = ReleaseVersion::from(version);
-        self.versions.contains_key(&release_version)
-    }
-
-    pub fn filter(self, version: Option<VersionReq>) -> Self {
-        if let Some(ref version) = version {
-            let versions = self.versions
-                .into_iter()
-                .filter(|(v, _)| version.matches(v.semver()))
-                .collect::<BTreeMap<_, _>>();
-
-            return Releases{ versions }
+        if let Some((release_version, mut release_info)) = it.next() {
+            release_info.version = Some(release_version.into());
+            manifest.latest = Some(release_info); 
         }
-        self
+
+        while let Some((release_version, mut release_info)) = it.next() {
+            release_info.version = Some(release_version.into());
+            manifest.versions.push(release_info); 
+        }
+
+        manifest
     }
 }
 
+#[serde_as]
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
 #[serde(default)]
 pub struct ReleaseInfo {
+    #[serde_as(as = "Option<DisplayFromStr>")]
+    version: Option<Version>,
     #[serde(flatten)]
     pub(crate) platforms: ExecutableTargets,
 }
@@ -215,7 +253,8 @@ pub(crate) fn exists(version: &Version) -> Result<bool> {
 /// Load the release definition JSON.
 pub(crate) fn load<P: AsRef<Path>>(target: P) -> Result<Releases> {
     let contents = fs::read_to_string(target.as_ref())?;
-    let releases: Releases = serde_json::from_str(&contents)?;
+    let manifest: ReleaseManifest = serde_json::from_str(&contents)?;
+    let releases: Releases = manifest.into();
     Ok(releases)
 }
 
@@ -230,12 +269,11 @@ pub(crate) fn save<P: AsRef<Path>>(
     target: P,
     releases: &Releases,
 ) -> Result<()> {
-    let contents = serde_json::to_vec_pretty(&releases)?;
+    let manifest: ReleaseManifest = releases.into();
     let mut file = File::create(target.as_ref())?;
-    file.write_all(contents.as_slice())?;
+    serde_json::to_writer_pretty(&mut file, &manifest)?;
     Ok(())
 }
-
 
 /// Get the path to the local releases repository.
 pub(crate) fn local_releases<P: AsRef<Path>>(
