a697035047ea8d8e3c7fb6c778b3158c3fe52433
muji <muji@tmpfs.org>
2021-01-18 10:52:43 +0800

    Complete initial region info mapping.

--- components/hosting/src/region_info.rs
+++ components/hosting/src/region_info.rs
@@ -59,49 +59,116 @@ pub static REGION_INFO: Lazy<HashMap<Region, RegionInfo>> = Lazy::new(|| {
         zone_id: Some("Z1WCIGYICN2BYD".to_string()),
     });
 
-    /*
-    m.insert(Region::CaCentral1, String::from(""));
-    m.insert(Region::EuCentral1, String::from(""));
-    m.insert(Region::EuWest1, String::from(""));
-    m.insert(Region::EuWest2, String::from(""));
-    m.insert(Region::EuWest3, String::from(""));
-    m.insert(Region::EuNorth1, String::from(""));
-    m.insert(Region::EuSouth1, String::from(""));
-    m.insert(Region::MeSouth1, String::from(""));
-    m.insert(Region::SaEast1, String::from(""));
-    m.insert(Region::UsEast1, String::from(""));
-    m.insert(Region::UsEast2, String::from(""));
-    m.insert(Region::UsWest1, String::from(""));
-    m.insert(Region::UsWest2, String::from(""));
-    m.insert(Region::UsGovEast1, String::from(""));
-    m.insert(Region::UsGovWest1, String::from(""));
-    m.insert(Region::CnNorth1, String::from(""));
-    m.insert(Region::CnNorthwest1, String::from(""));
-    m.insert(Region::AfSouth1, String::from(""));
-    */
+    m.insert(Region::CaCentral1, RegionInfo {
+        title: "Canada (Central)".to_string(),
+        s3_endpoint_suffix: "s3-website.ca-central-1.amazonaws.com".to_string(),
+        zone_id: Some("Z1QDHH18159H29".to_string()),
+    });
 
-    /*
-Region Name 	Website Endpoint 	Route 53 Hosted Zone ID
+    m.insert(Region::EuCentral1, RegionInfo {
+        title: "Europe (Frankfurt)".to_string(),
+        s3_endpoint_suffix: "s3-website.eu-central-1.amazonaws.com".to_string(),
+        zone_id: Some("Z21DNDUVLTQW6Q".to_string()),
+    });
 
-US East (Ohio) 	s3-website.us-east-2.amazonaws.com 	Z2O1EMRO9K5GLX
-US East (N. Virginia) 3-website-us-east-1.amazonaws.com Z3AQBSTGFYJSTF 
-US West (N. California) s3-website-us-west-1.amazonaws.com Z2F56UZL2M1ACD 
-US West (Oregon) s3-website-us-west-2.amazonaws.com Z3BJ6K6RIION7M
-Africa (Cape Town) s3-website.af-south-1.amazonaws.com Z11KHD8FBVPUYU
-Canada (Central) s3-website.ca-central-1.amazonaws.com Z1QDHH18159H29
-China (Ningxia) s3-website.cn-northwest-1.amazonaws.com.cn Not supported
-Europe (Frankfurt) s3-website.eu-central-1.amazonaws.com Z21DNDUVLTQW6Q
-Europe (Ireland) s3-website-eu-west-1.amazonaws.com Z1BKCTXD74EZPE
-Europe (London) s3-website.eu-west-2.amazonaws.com Z3GKZC51ZF0DB4
-Europe (Milan) s3-website.eu-south-1.amazonaws.com Not supported
-Europe (Paris) s3-website.eu-west-3.amazonaws.com Z3R1K369G5AVDG
-Europe (Stockholm) s3-website.eu-north-1.amazonaws.com Z3BAZG2TWCNX0D
-Middle East(Bahrain) s3-website.me-south-1.amazonaws.com Not supported
-South America (São Paulo) s3-website-sa-east-1.amazonaws.com Z7KQH4QJS55SO
-AWS GovCloud (US-East) s3-website.us-gov-east-1.amazonaws.com Z2NIFVYYW2VKV1
-AWS GovCloud (US) s3-website-us-gov-west-1.amazonaws.com Z31GFT0UA1I2HV
-*/
+    m.insert(Region::EuWest1, RegionInfo {
+        title: "Europe (Ireland)".to_string(),
+        s3_endpoint_suffix: "s3-website-eu-west-1.amazonaws.com".to_string(),
+        zone_id: Some("Z1BKCTXD74EZPE".to_string()),
+    });
 
+    m.insert(Region::EuWest2, RegionInfo {
+        title: "Europe (London)".to_string(),
+        s3_endpoint_suffix: "s3-website.eu-west-2.amazonaws.com".to_string(),
+        zone_id: Some("Z3GKZC51ZF0DB4".to_string()),
+    });
+
+    m.insert(Region::EuWest3, RegionInfo {
+        title: "Europe (Paris)".to_string(),
+        s3_endpoint_suffix: "s3-website.eu-west-3.amazonaws.com".to_string(),
+        zone_id: Some("Z3R1K369G5AVDG".to_string()),
+    });
+
+    m.insert(Region::EuNorth1, RegionInfo {
+        title: "Europe (Stockholm)".to_string(),
+        s3_endpoint_suffix: "s3-website.eu-north-1.amazonaws.com".to_string(),
+        zone_id: Some("Z3BAZG2TWCNX0D".to_string()),
+    });
+
+    m.insert(Region::EuSouth1, RegionInfo {
+        title: "Europe (Milan)".to_string(),
+        s3_endpoint_suffix: "s3-website.eu-south-1.amazonaws.com".to_string(),
+        zone_id: None,
+    });
+
+    m.insert(Region::MeSouth1, RegionInfo {
+        title: "Middle East(Bahrain)".to_string(),
+        s3_endpoint_suffix: "s3-website.me-south-1.amazonaws.com".to_string(),
+        zone_id: None,
+    });
+
+    m.insert(Region::SaEast1, RegionInfo {
+        title: "South America (São Paulo)".to_string(),
+        s3_endpoint_suffix: "s3-website-sa-east-1.amazonaws.com".to_string(),
+        zone_id: Some("Z7KQH4QJS55SO".to_string()),
+    });
+
+    m.insert(Region::UsEast1, RegionInfo {
+        title: "US East (N. Virginia)".to_string(),
+        s3_endpoint_suffix: "s3-website-us-east-1.amazonaws.com".to_string(),
+        zone_id: Some("Z3AQBSTGFYJSTF".to_string()),
+    });
+
+    m.insert(Region::UsEast2, RegionInfo {
+        title: "US East (Ohio)".to_string(),
+        s3_endpoint_suffix: "s3-website.us-east-2.amazonaws.com".to_string(),
+        zone_id: Some("Z2O1EMRO9K5GLX".to_string()),
+    });
+
+    m.insert(Region::UsWest1, RegionInfo {
+        title: "US West (N. California)".to_string(),
+        s3_endpoint_suffix: "s3-website-us-west-1.amazonaws.com".to_string(),
+        zone_id: Some("Z2F56UZL2M1ACD".to_string()),
+    });
+
+    m.insert(Region::UsWest2, RegionInfo {
+        title: "US West (Oregon)".to_string(),
+        s3_endpoint_suffix: "s3-website-us-west-2.amazonaws.com".to_string(),
+        zone_id: Some("Z3BJ6K6RIION7M".to_string()),
+    });
+
+    m.insert(Region::UsGovEast1, RegionInfo {
+        title: "AWS GovCloud (US-East)".to_string(),
+        s3_endpoint_suffix: "s3-website.us-gov-east-1.amazonaws.com".to_string(),
+        zone_id: Some("Z2NIFVYYW2VKV1".to_string()),
+    });
+
+    m.insert(Region::UsGovWest1, RegionInfo {
+        title: "AWS GovCloud (US)".to_string(),
+        s3_endpoint_suffix: "s3-website-us-gov-west-1.amazonaws.com".to_string(),
+        zone_id: Some("Z31GFT0UA1I2HV".to_string()),
+    });
+
+    // WARN: An S3 endpoint is not documented for `cn-north-1`
+    // WARN: but we follow the convention for `cn-northwest-1` 
+    // WARN: so that we don't have to make it Option<String>.
+    m.insert(Region::CnNorth1, RegionInfo {
+        title: "China (Beijing)".to_string(),
+        s3_endpoint_suffix: "s3-website.cn-north-1.amazonaws.com.cn".to_string(),
+        zone_id: None,
+    });
+
+    m.insert(Region::CnNorthwest1, RegionInfo {
+        title: "China (Ningxia)".to_string(),
+        s3_endpoint_suffix: "s3-website.cn-northwest-1.amazonaws.com.cn".to_string(),
+        zone_id: None,
+    });
+
+    m.insert(Region::AfSouth1, RegionInfo {
+        title: "Africa (Cape Town)".to_string(),
+        s3_endpoint_suffix: "s3-website.af-south-1.amazonaws.com".to_string(),
+        zone_id: Some("Z11KHD8FBVPUYU".to_string()),
+    });
     m
 });
 
