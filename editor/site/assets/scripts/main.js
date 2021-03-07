7cc0188f85ce6c57b0d23c78355d69654b01db09
muji <muji@tmpfs.org>
2021-03-07 13:04:27 +0800

    Test for dropping files into webview.

--- editor/site/assets/scripts/main.js
+++ editor/site/assets/scripts/main.js
@@ -1,7 +1,7 @@
 const embedded = typeof rpc !== 'undefined';
 
 if (embedded) {
-  const tee = false;
+  const tee = true;
 
   const console_methods = {
     log: console.log,
@@ -78,6 +78,38 @@ async function chooseProject() {
     console.error("Got error choosing folder", e);
   }
 }
+
+/*
+function dragOverHandler(ev) {
+  //console.log('File(s) in drop zone');
+
+  // Prevent default behavior (Prevent file from being opened)
+  ev.preventDefault();
+}
+
+function dropHandler(ev) {
+  console.log('File(s) dropped');
+
+  // Prevent default behavior (Prevent file from being opened)
+  ev.preventDefault();
+
+  if (ev.dataTransfer.items) {
+    // Use DataTransferItemList interface to access the file(s)
+    for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+      // If dropped items aren't files, reject them
+      if (ev.dataTransfer.items[i].kind === 'file') {
+        var file = ev.dataTransfer.items[i].getAsFile();
+        console.log('... file[' + i + '].name = ' + file.name);
+      }
+    }
+  } else {
+    // Use DataTransfer interface to access the file(s)
+    for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+      console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+    }
+  }
+}
+*/
 
 /*
 console.info('App started (info)');
