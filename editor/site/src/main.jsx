69a0f9eefe2c7db943d1484412c780b612e6d283
muji <muji@tmpfs.org>
2021-03-08 08:36:33 +0800

    Test 'contextmenu' event handling.

--- editor/site/src/main.jsx
+++ editor/site/src/main.jsx
@@ -2,4 +2,12 @@ import { h, Fragment, render, createContext } from 'preact';
 import * as _ from './preamble';
 import App from './App';
 import {value, State} from './State';
+
+/*
+window.addEventListener('contextmenu', (e) => {
+  console.log('context menu event');
+  e.preventDefault();
+});
+*/
+
 render(<State.Provider value={value}><App /></State.Provider>, document.body);
